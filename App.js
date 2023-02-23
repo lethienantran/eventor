6be6badcce257dad8da5efb394c0c6213b1fd475
tranleduyan <leduyantran@gmail.com>
2023-02-23 00:07:50 -0800

    update a bit of eventdetailscreen.js - leduyantran

--- App.js
+++ App.js
@@ -9,24 +9,30 @@ import HomeScreen from './src/Screens/HomeScreen';
 import LandingScreen from './src/Screens/LandingScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {StatusBar, View} from 'react-native';
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar
+        backgroundColor="#FFF"
+        barStyle="dark-content"
+      />
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="LandingScreen" component={LandingScreen} />
-        <Stack.Screen name="AddEventScreen" component={AddEventScreen} />
-        <Stack.Screen name="AddTaskScreen" component={AddTaskScreen} />
-        <Stack.Screen name="EditEventScreen" component={EditEventScreen} />
-        <Stack.Screen name="EditTaskScreen" component={EditTaskScreen} />
-        <Stack.Screen name="EventDetailScreen" component={EventDetailScreen} />
-        <Stack.Screen
-          name="FilteredEventScreen"
-          component={FilteredEventScreen}
-        />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="LandingScreen" component={LandingScreen} />
+      <Stack.Screen name="AddEventScreen" component={AddEventScreen} />
+      <Stack.Screen name="AddTaskScreen" component={AddTaskScreen} />
+      <Stack.Screen name="EditEventScreen" component={EditEventScreen} />
+      <Stack.Screen name="EditTaskScreen" component={EditTaskScreen} />
+      <Stack.Screen name="EventDetailScreen" component={EventDetailScreen} />
+      <Stack.Screen
+        name="FilteredEventScreen"
+        component={FilteredEventScreen}
+      />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/Screens/EventDetailScreen/EventDetailScreen.js
+++ src/Screens/EventDetailScreen/EventDetailScreen.js
@@ -1,16 +1,46 @@
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, Pressable} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Feather from 'react-native-vector-icons/Feather';
+import { ScaledSheet } from 'react-native-size-matters';
+import { RFPercentage } from 'react-native-responsive-fontsize';
 const {height, width} = Dimensions.get('window');
 
 const AddEventScreen = () => {
+
+  const onBackPressed = () =>{
+
+  };
+
+  const onEditPressed = () =>{
+
+  };
+
   //call useNavigation to be able to navigate around
   const navigation = useNavigation();
   return (
     <View style={styles.root}>
       <View style={styles.contentContainer}>
         <View style={styles.headerContainer}>
-          
+          <Pressable style={styles.backButton} onPress={onBackPressed}>
+            <Entypo
+              name="chevron-left"
+              style={styles.backIcon}
+              color={'#000'}
+            />
+          </Pressable>
+          <Text style={styles.logoText}>eventor</Text>
+          <Pressable style={styles.editButton} onPress={onEditPressed}>
+            <Feather
+              name="edit"
+              style={styles.editIcon}
+              color={'#000'}
+            />
+          </Pressable>
+        </View>
+        <View style={styles.eventBannerContainer}>
+
         </View>
       </View>
     </View>
@@ -23,20 +53,48 @@ const styles = StyleSheet.create({
     flexDirection:'row',
     justifyContent:'center',
     alignItems:'center',
-    backgroundColor:'yellow',
+    backgroundColor:'#FFF',
   },
   contentContainer:{
     width: width - 50,
     height: height,
     flexDirection:'column',
-    backgroundColor:'red',
   },
   headerContainer:{
     flexDirection:'row',
     justifyContent:'space-between',
+    alignItems:'center',
     width:'100%',
-    height:'10%',
-    backgroundColor:'green',
+    height:80,
+  },
+  backButton:{
+    height:36,
+    width:36,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  backIcon:{
+    fontSize:20,
+  },
+  logoText:{
+    fontSize: RFPercentage(4),
+    fontFamily: 'OpenSans-SemiBold',
+    color: 'black',
+  },
+  editButton:{
+    height:36,
+    width:36,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  editIcon:{
+    fontSize:20,
+  },
+  eventBannerContainer:{
+    width:'100%',
+    height: 113,
+    borderRadius:20,
+    backgroundColor:'#EEEEEE',
   },
 });
 export default AddEventScreen;
