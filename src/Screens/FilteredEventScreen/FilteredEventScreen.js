5c9c0596cd15be53b1912911105ae9aaac48d766
Johnnie <johnnieann7@gmail.com>
2023-03-03 01:43:20 -0800

    complete populate flatList for filteredEventScreen TODO://complete filter feature of the page Let's GOOOO

--- src/Screens/FilteredEventScreen/FilteredEventScreen.js
+++ src/Screens/FilteredEventScreen/FilteredEventScreen.js
@@ -1,64 +1,35 @@
 import {View, Text, Pressable, FlatList, ImageBackground} from 'react-native';
-import React, { useEffect, useState, useContext} from 'react';
+import React, { useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import { ScaledSheet } from 'react-native-size-matters';
 import Logo from '../../components/Logo';
 import ViewModeButton from '../../components/ViewModeButton';
 import { RFPercentage } from 'react-native-responsive-fontsize';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import {DBContext} from '../../../App';
+import { useIsFocused } from '@react-navigation/native';
+import SQLite from 'react-native-sqlite-storage';
 import moment from 'moment';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
-const eventsData= [
-  {id: '1', name: 'Brain Injury Art Show', progress: 67, date: '2/26/2023 8:00'},
-  {id: '2', name: 'Seattle Asian American...', progress: 23, date: '03/01/2023 18:00'},
-  {id: '3', name: 'Dave Holland Trio', progress: 100, date: '02/05/2023 20:00'},
-  {id: '4', name: 'RUBBERBAND - Reckless...', progress: 100, date: '01/29/2023 17:00'},
-  {id: '5', name: 'Washington: My Home', progress: 100, date: '12/29/2022 8:00'}
-];
-
+import Feather from 'react-native-vector-icons/Feather';
+import Loading from '../../components/Loading';
   const FilteredEventScreen = () => {
     const navigation = useNavigation();
-    const [viewMode, setViewMode] = useState('All');
-    const [events, setevents] = useState(eventsData);
-
-
-  const renderEmptyComponent = () => (
-    <View style={styles.emptyContainer}>
-      <Text>No events to show.</Text>
-    </View>
-  );
-
-  const filteredEvents = () => {
-    const now = new Date();
-    switch (viewMode) {
-      case 'Past':
-        return events.filter((event) => new Date(event.date) < now && event.progress === 100);
-      case 'Complete':
-        return events.filter((event) => event.progress === 100);
-      case 'In-Progress':
-        return events.filter((event) => event.progress > 0 && event.progress < 100);
-      default:
-        return events;
-    }
-  };
-
+    const [data, setData] = useState([]);
+    const isFocused = useIsFocused();
+    const [isLoading, setIsLoading] = useState(true);
+    const [filterCase, setFilterCase] = useState('All');
 
   const onAllPressed = () => {
-    setViewMode('All');
+    setFilterCase('All');
   };
   const onPastPressed = () => {
-    setViewMode('Past');
+    setFilterCase('Past');
   };
   const onCompletePressed = () => {
-    setViewMode('Complete');
+    setFilterCase('Complete');
   };
   const onInProgressPressed = () => {
-    setViewMode('In-Progress');
+    setFilterCase('In-Progress');
   };
 
   const onBackPressed = () => {
@@ -70,83 +41,146 @@ const eventsData= [
     
   };
   const onEventPressed = async selectedEventID => {
-    AsyncStorage.setItem('selectedEventID', selectedEventID.toString())
-      .then(() => {
-        console.log('SelectedEventID: ' + selectedEventID.toString());
-        navigation.navigate('EventDetailScreen');
-      })
-      .catch(error => {
-        console.error(
-          'Error',
-          'Could not save SelectedEventID to AsyncStorage!',
+  };
+    //run query
+    useEffect(() => {
+      if (isFocused) {
+        setIsLoading(true);
+        const db = SQLite.openDatabase(
+          {
+            name: 'eventorDB.db',
+            createFromLocation: 1,
+          },
+          () => {
+            console.log('FilteredEventScreen: Database opened successfully');
+            db.transaction(tx => {
+              //for populating all event
+              tx.executeSql(
+                `SELECT * FROM events ORDER BY eventEndTime DESC`,
+                [],
+                (tx, results) => {
+                  var temp = [];
+                  for (let i = 0; i < results.rows.length; ++i) {
+                    temp.push(results.rows.item(i));
+                  }
+                  setData(temp);
+                  setIsLoading(false);
+                  db.close();
+                },
+              );
+            });
+          },
+          error => {
+            console.log(error);
+          },
         );
-        console.error(error);
-      });
-  };
-  
+      }
+    }, [isFocused]);
 
-  const renderItem = ({ item }) => {
-    const eventStartTime = moment(item.date, 'MM/DD/YYYY HH:mm').format('MM/DD/YYYY');
-    const eventEndTime = moment(item.date, 'MM/DD/YYYY HH:mm').add(1, 'day').format('MM/DD/YYYY');
-    
-    return (
-      <TouchableOpacity
-        key={item.id}
-        style={styles.feedItem}
-        activeOpacity={0.7}
-        onPress={() => onEventPressed(item.id)}>
-        <View style={styles.dateTimeContainer}>
-          <Text style={styles.dateTimeText}>
-            {eventStartTime} - {eventEndTime}
-          </Text>
-        </View>
-        <View style={styles.titleItemContainer}>
-          <Text style={styles.titleItemText}>{item.name}</Text>
-        </View>
-        <View style={styles.progressionItemContainer}>
-          <Text style={styles.progressionItemText}>Progress:</Text>
-          <Text style={styles.displayProgressionItemText}>
-            {item.progress}%
-          </Text>
-        </View>
-      </TouchableOpacity>
-    );
-  };
-  
-  return (
-      <View style={styles.root}>
-        <View style={styles.container}>
-          <Logo hasBack={true} onPress={onBackPressed}/>
-          <View style={styles.contentContainer}>
-            <View style={styles.viewModeContainer}>
-              <ViewModeButton mode='All' viewMode={viewMode} title='All' type='EvenSpace' onPress={onAllPressed}/>
-              <ViewModeButton mode='Past' viewMode={viewMode} title='Past' type='EvenSpace' onPress={onPastPressed}/>
+    //listItemView for FlatList (item/cards) show on screen
+    const listItemView = item => {
+      //format dateTime
+      const eventStartTime = moment(new Date(item.eventStartTime)).format(
+        'MM/DD/YYYY HH:mm',
+      );
+      const eventEndTime = moment(new Date(item.eventEndTime)).format(
+        'MM/DD/YYYY HH:mm',
+      );
+      return (
+        <TouchableOpacity
+          key={item.eventID}
+          style={styles.feedItem}
+          activeOpacity={0.7}
+          onPress={() => onEventPressed(item.eventID)}>
+          <ImageBackground
+            source={
+              item.eventImage !== null
+                ? {uri: `data:image/jpeg;base64,${item.eventImage}`}
+                : require('../../../assets/images/eventsBanner.png')
+            }
+            resizeMode="cover"
+            style={{flex: 1}}
+            imageStyle={styles.itemImage}
+            blurRadius={5}>
+            <View style={styles.dateTimeContainer}>
+              <Text style={styles.dateTimeText}>
+                {eventStartTime} - {eventEndTime}
+              </Text>
             </View>
-            <View style={styles.viewModeContainer}>
-              <ViewModeButton mode='Complete' viewMode={viewMode} title='Complete' type='EvenSpace' onPress={onCompletePressed}/>
-              <ViewModeButton mode='In-Progress' viewMode={viewMode} title='In-Progress' type='EvenSpace' onPress={onInProgressPressed}/>
+            <View style={styles.titleItemContainer}>
+              <Text style={styles.titleItemText}>{item.eventName}</Text>
             </View>
-            <View style={styles.viewTitleContainer}>
-              <Text style={styles.viewTitleText}>{viewMode} Events</Text>
+            <View style={styles.progressionItemContainer}>
+              <Text style={styles.progressionItemText}>Progress:</Text>
+              <Text style={styles.displayProgressionItemText}>
+                {item.eventProgress}%
+              </Text>
             </View>
-            <FlatList
-             data={filteredEvents()}
-             renderItem={renderItem}
-             keyExtractor={(item) => item.id}
-             ListEmptyComponent={renderEmptyComponent}
-             showsVerticalScrollIndicator={false}
-            />
+          </ImageBackground>
+        </TouchableOpacity>
+      );
+    };
+
+  const display = () => {
+    if(!isLoading)
+    {
+      return(
+        <>
+          <View style={styles.container}>
+            <Logo hasBack={true} onPress={onBackPressed}/>
+            <View style={styles.contentContainer}>
+              <View style={styles.viewModeContainer}>
+                <ViewModeButton mode='All' viewMode={filterCase} title='All' type='EvenSpace' onPress={onAllPressed}/>
+                <ViewModeButton mode='Past' viewMode={filterCase} title='Past' type='EvenSpace' onPress={onPastPressed}/>
+              </View>
+              <View style={styles.viewModeContainer}>
+                <ViewModeButton mode='Complete' viewMode={filterCase} title='Complete' type='EvenSpace' onPress={onCompletePressed}/>
+                <ViewModeButton mode='In-Progress' viewMode={filterCase} title='In-Progress' type='EvenSpace' onPress={onInProgressPressed}/>
+              </View>
+              <View style={styles.viewTitleContainer}>
+                <Text style={styles.viewTitleText}>{filterCase} Events</Text>
+              </View>
+              <View style = {styles.feedContainer}>
+              {data.length != 0 ? (
+                  <FlatList
+                    showsVerticalScrollIndicator={false}
+                    data={data}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({item}) => listItemView(item)}
+                  />
+                ) : (
+                  <View style = {styles.noEventOutsideContainer}>
+                  <View style={styles.noEventContainer}>
+                    <Feather name='check-circle' style={styles.checkCircleIcon}/>
+                    <Text style = {styles.noEventText}>You don't have any events yet!</Text>
+                  </View>
+                  </View>
+                )}
+              </View>
           </View>
         </View>
         <View style = {styles.addButtonIcon}>
-        <Pressable onPress={addButtonPressed}>
-        <MaterialCommunityIcons
-          name="plus-circle"
-          color={'#FF3008'}
-          size = {100}
-        />
-      </Pressable>
+          <Pressable onPress={addButtonPressed}>
+            <MaterialCommunityIcons
+              name="plus-circle"
+              color={'#FF3008'}
+              size = {100}
+            />
+          </Pressable>
         </View>
+        </>
+      )
+    }
+    else
+    {
+      return(
+        <Loading />
+      )
+    }
+  }
+  return (
+      <View style={styles.root}>
+        {display()}
       </View>
     );
   };
@@ -202,7 +236,7 @@ const styles = ScaledSheet.create({
   },
   feedContainer: {
     width: '100%',
-    height: '40%',
+    height: '60%',
   },
   feedItem: {
     alignSelf: 'center',
@@ -257,7 +291,29 @@ const styles = ScaledSheet.create({
     color: 'white',
     fontSize: RFPercentage(1.8),
   },
-  
+  noEventOutsideContainer:{
+    width:"100%",
+    height:"100%",
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  noEventContainer:{
+    width:"85%",
+    height:"100%",
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  checkCircleIcon:{
+    fontSize:RFPercentage(8),
+    color:'#21B608',
+    alignSelf:'center',
+    marginBottom:"4@vs",
+  },
+  noEventText:{
+    fontFamily:"OpenSans-Regular",
+    fontSize:RFPercentage(2.5),
+    color:"black",
+  }
 });
 
 export default FilteredEventScreen;
